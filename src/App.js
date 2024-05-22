1ca96024771e22a9d2df186706e87df288b63bfe
pradyumna <pradyusikhakolli12345@gmail.com>
2024-05-22 15:45:29 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { displayResultString: "0", string: "" };
+
+  check = (no) => {
+    const { string } = this.state;
+    let tempString = string + no;
+
+    this.setState({ string: tempString, displayResultString: tempString });
+  };
+
+  clickedResult = () => {
+    const { string } = this.state;
+    const result = eval(string);
+    this.setState({
+      string: result.toString(),
+      displayResultString: result.toString(),
+    });
+  };
+
+  clickedSingleClear = () => {
+    const { string } = this.state;
+    let tempString = string;
+    tempString = tempString.slice(0, tempString.length - 1);
+    this.setState({ displayResultString: tempString, string: tempString });
+  };
+
+  clickedClear = () => {
+    this.setState({ displayResultString: "0", string: "" });
+  };
+
+  render() {
+    const { displayResultString } = this.state;
+    return (
+      <div className="calculator-bg-container">
+        <div className="calculator-container">
+          <p className="result" id="resultElement">
+            {displayResultString}
+          </p>
+          <div className="container">
+            <div className="row">
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("7")}
+                >
+                  7
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("8")}
+                >
+                  8
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("9")}
+                >
+                  9
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button operator-bg"
+                  onClick={() => this.check("+")}
+                >
+                  +
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("4")}
+                >
+                  4
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("5")}
+                >
+                  5
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("6")}
+                >
+                  6
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button operator-bg"
+                  onClick={() => this.check("-")}
+                >
+                  -
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("1")}
+                >
+                  1
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("2")}
+                >
+                  2
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("3")}
+                >
+                  3
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button operator-bg"
+                  onClick={() => this.check("*")}
+                >
+                  *
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={() => this.check("0")}
+                >
+                  0
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={this.clickedSingleClear}
+                >
+                  DEL
+                </button>
+              </div>
+
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button"
+                  onClick={this.clickedClear}
+                >
+                  AC
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button operator-bg"
+                  onClick={() => this.check("/")}
+                >
+                  /
+                </button>
+              </div>
+              <div className="col-3 c-button-container">
+                <button
+                  className="calculator-button operator-bg"
+                  onClick={() => this.check(".")}
+                >
+                  .
+                </button>
+              </div>
+              <div className="col-9 c-button-container">
+                <button
+                  className="calculator-button equalto-button"
+                  onClick={() => this.clickedResult()}
+                >
+                  =
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
